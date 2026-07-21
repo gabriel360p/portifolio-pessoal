38530bb2b13a46201c54aa2175b0f35fa34fa9ba
gabriel360p <gabrielmark210@gmail.com>
2026-07-20 22:32:04 -0300

    fix: correções mínimas

--- src/components/Modal/ModalCertificate.tsx
+++ src/components/Modal/ModalCertificate.tsx
@@ -6,7 +6,7 @@ interface ModalCertificateProps {
 export function ModalCertificate({ certificate }: ModalCertificateProps) {
     return (
         <div>
-            
+            <h2 className="font-bold">Teste Certificado:{certificate.title}</h2> 
         </div>
     )
 }
--- src/components/Modal/ModalProject.tsx
+++ src/components/Modal/ModalProject.tsx
@@ -4,8 +4,10 @@ interface ModalCeProjectProps {
     projeto: Project;
 }
 export function ModalProject({ projeto }: ModalCeProjectProps) {
+    console.log(projeto)
     return (
-        <>
-        </>
+        <div> 
+            <h2 className="font-bold">Teste Projeto:{projeto.title}</h2>
+        </div>
     )
 }
